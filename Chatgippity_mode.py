44315473696f8acd173152c8418bb280fb0d431b
Bastian Strube <bastian.strube@wombogames.com>
2025-02-06 18:09:19 +0100

    another refactor from favorite overlord

--- Chatgippity_mode.py
+++ Chatgippity_mode.py
@@ -235,3 +235,199 @@ class VIEW3D_PIE_MT_mode(Menu):
 
     def handle_default_mode(self, context, pie):
         pie.separator()
+
+
+
+
+
+#########################################################
+################ CLAIMED IMPROVEMENTS AND COMMENTS
+
+
+import bpy
+from bpy.types import Menu
+
+class VIEW3D_PIE_MT_mode(Menu):
+    bl_label = "Mode Selection"
+
+    def draw(self, context):
+        mode_handlers = {
+            'OBJECT': self.object_mode_pie,
+            'EDIT_MESH': self.edit_mesh_pie,
+            'EDIT_CURVE': self.edit_curve_pie,
+            'EDIT_GPENCIL': self.edit_gpencil_pie,
+            'PAINT_GREASE_PENCIL': self.paint_grease_pencil_pie,
+            'SCULPT_GREASE_PENCIL': self.sculpt_grease_pencil_pie,
+            'PAINT_TEXTURE': self.paint_texture_pie,
+            'PAINT_VERTEX': self.paint_vertex_pie,
+            'SCULPT': self.sculpt_pie,
+            'POSE': self.pose_pie,
+            'EDIT_LATTICE': self.edit_lattice_pie,
+            'EDIT_ARMATURE': self.edit_armature_pie
+        }
+
+        handler = mode_handlers.get(context.mode, self.default_pie)
+        handler(context)
+
+    def object_mode_pie(self, context):
+        """Pie menu for OBJECT mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+        obj = context.object
+
+        # NORTH
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+        # SOUTH
+        pie.menu("VIEW3D_MT_object_context_menu", text="Object Menu")
+
+        # EAST
+        subPie = pie.operator("wm.call_menu_pie", text='Select...')
+        subPie.name = "SUBPIE_MT_objectSelect"
+
+        # WEST
+        if obj and obj.type in {'CURVE', 'SURFACE', 'LATTICE', 'ARMATURE'}:
+            pie.separator()
+
+    def edit_mesh_pie(self, context):
+        """Pie menu for EDIT MESH mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch back to Object Mode
+        pie.operator("object.mode_set", text="Object Mode", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Edge Selection
+        pie.operator('mesh.select_mode', text="Edge", icon="EDGESEL").type = 'EDGE'
+
+        # EAST - Vertex Selection
+        pie.operator('mesh.select_mode', text="Vertex", icon="VERTEXSEL").type = 'VERT'
+
+        # WEST - Face Selection
+        pie.operator('mesh.select_mode', text="Face", icon="FACESEL").type = 'FACE'
+
+        # NW - Edge Menu
+        pie.menu("VIEW3D_MT_edit_mesh_edges", text="Edge Menu", icon="COLLAPSEMENU")
+
+        # NE - Vertex Menu
+        pie.menu("VIEW3D_MT_edit_mesh_vertices", text="Vert Menu", icon="COLLAPSEMENU")
+
+        # SW - Face Menu
+        pie.menu("VIEW3D_MT_edit_mesh_faces", text="Face Menu", icon="COLLAPSEMENU")
+
+        # SE - Select Menu
+        pie.operator("wm.call_menu_pie", text='Select...').name = "SUBPIE_MT_meshSelect"
+
+    def edit_curve_pie(self, context):
+        """Pie menu for EDIT CURVE mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Curve Menu
+        pie.menu("VIEW3D_MT_edit_curve_context_menu", text="Curve Menu", icon="COLLAPSEMENU")
+
+        # EAST - Change Curve Type
+        pie.operator("wm.call_menu_pie", text='Curve/Handle Type...').name = "SUBPIE_MT_curveTypeHandles"
+
+        # WEST - Curve Operations
+        pie.operator("curve.cyclic_toggle")
+        pie.operator("curve.switch_direction")
+
+    def edit_gpencil_pie(self, context):
+        """Pie menu for EDIT GREASE PENCIL mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+    def paint_grease_pencil_pie(self, context):
+        """Pie menu for PAINT GREASE PENCIL mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+    def sculpt_grease_pencil_pie(self, context):
+        """Pie menu for SCULPT GREASE PENCIL mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+    def paint_texture_pie(self, context):
+        """Pie menu for TEXTURE PAINT mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", text="Object Mode", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Separator
+        pie.separator()
+
+        # CENTER - Empty Box (for future expansion)
+        pie.box()
+
+    def paint_vertex_pie(self, context):
+        """Pie menu for VERTEX PAINT mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", text="Object Mode", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Separator
+        pie.separator()
+
+        # CENTER - Empty Box (for future expansion)
+        pie.box()
+
+    def sculpt_pie(self, context):
+        """Pie menu for SCULPT mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", text="Object Mode", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Toggle Dyntopo
+        pie.operator("sculpt.dynamic_topology_toggle", text="Dyntopo Toggle")
+
+        # CENTER - Empty Box (for future expansion)
+        pie.box()
+
+    def pose_pie(self, context):
+        """Pie menu for POSE mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Pose Context Menu
+        pie.menu("VIEW3D_MT_pose_context_menu", text="Pose Context Menu", icon="COLLAPSEMENU")
+
+    def edit_lattice_pie(self, context):
+        """Pie menu for EDIT LATTICE mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+
+    def edit_armature_pie(self, context):
+        """Pie menu for EDIT ARMATURE mode"""
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        # NORTH - Switch to Object Mode
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+
+        # SOUTH - Armature Context Menu
+        pie.menu("VIEW3D_MT_edit_armature_names")
+
+    def default_pie(self, context):
+        """Fallback pie menu if mode is not recognized"""
+        layout = self.layout
+        pie = layout.menu_pie()
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
