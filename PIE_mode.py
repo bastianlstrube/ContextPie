519bc86fd7c76208b6e341e788590dc2ff9dfec3
Bastian Strube <bastian.strube@wombogames.com>
2025-02-07 17:42:06 +0100

    refactor to functions instead of if/elif

--- PIE_mode.py
+++ PIE_mode.py
@@ -126,6 +126,196 @@ class SUBPIE_MT_curveTypeHandles(Menu):
         pie.operator("curve.spline_type_set", text='NURBS Curve').type = 'NURBS'
 
 class VIEW3D_PIE_MT_mode(Menu):
+    bl_label = "Mode Selection"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator_context = 'INVOKE_REGION_WIN'
+        pie = layout.menu_pie()
+
+        mode_actions = {
+            'OBJECT': self.draw_object_mode,
+            'EDIT_MESH': self.draw_edit_mesh_mode,
+            'EDIT_CURVE': self.draw_edit_curve_mode,
+            'EDIT_GPENCIL': self.draw_edit_gpencil_mode,
+            'EDIT_GREASE_PENCIL': self.draw_edit_gpencil_mode,
+            'PAINT_GREASE_PENCIL': self.draw_paint_gpencil_mode,
+            'SCULPT_GREASE_PENCIL': self.draw_sculpt_gpencil_mode,
+            'PAINT_TEXTURE': self.draw_paint_texture_mode,
+            'PAINT_WEIGHT': self.draw_paint_weight_mode,
+            'SCULPT': self.draw_sculpt_mode,
+            'POSE': self.draw_pose_mode,
+            'EDIT_LATTICE': self.draw_edit_lattice_mode,
+            'EDIT_ARMATURE': self.draw_edit_armature_mode,
+            'PAINT_VERTEX': self.draw_paint_vertex_mode
+        }
+
+        if context.mode in mode_actions:
+            mode_actions[context.mode](pie, context)
+
+    def draw_object_mode(self, pie, context):
+        obj = context.object
+        if obj and obj.type in {'MESH', 'GPENCIL', 'GREASEPENCIL', 'FONT'}:
+            pie.operator_enum("OBJECT_OT_mode_set", "mode")
+            pie.menu("VIEW3D_MT_object_context_menu", text="Object Menu")
+            subPie = pie.operator("wm.call_menu_pie", text='Select...')
+            subPie.name = "SUBPIE_MT_objectSelect"
+        elif obj and obj.type in {'CURVE', 'SURFACE', 'LATTICE'}:
+            pie.operator_enum("OBJECT_OT_mode_set", "mode")
+            pie.separator()
+            pie.separator()
+            pie.separator()
+            pie.separator()
+            pie.menu("VIEW3D_MT_object_context_menu", text="Object Menu")
+            subPie = pie.operator("wm.call_menu_pie", text='Select...')
+            subPie.name = "SUBPIE_MT_objectSelect"
+        elif obj and obj.type == 'ARMATURE':
+            pie.operator_enum("OBJECT_OT_mode_set", "mode")
+            pie.separator()
+            pie.separator()
+            pie.separator()
+            pie.menu("VIEW3D_MT_object_context_menu", text="Object Menu")
+            subPie = pie.operator("wm.call_menu_pie", text='Select...')
+            subPie.name = "SUBPIE_MT_objectSelect"
+
+    def draw_edit_mesh_mode(self, pie, context):
+        pie.operator("object.mode_set", text="object mode", icon="OBJECT_DATAMODE")
+        pie.operator('mesh.select_mode', text="Vertex", icon="VERTEXSEL").type = 'VERT'
+        pie.operator('mesh.select_mode', text="Face", icon="FACESEL").type = 'FACE'
+        pie.operator('mesh.select_mode', text="Edge", icon="EDGESEL").type = 'EDGE'
+        pie.menu("VIEW3D_MT_edit_mesh_edges", text="edge menu", icon="COLLAPSEMENU")
+        pie.menu("VIEW3D_MT_edit_mesh_vertices", text="vert menu", icon="COLLAPSEMENU")
+        pie.menu("VIEW3D_MT_edit_mesh_faces", text="face menu", icon="COLLAPSEMENU")
+        pie.operator("wm.call_menu_pie", text='Select...').name = "SUBPIE_MT_meshSelect"
+
+    def draw_edit_curve_mode(self, pie, context):
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+        pie.menu("VIEW3D_MT_edit_curve_context_menu", text="curve menu", icon="COLLAPSEMENU")
+        pie.operator("wm.call_menu_pie", text='Curve/Handle Type...').name = "SUBPIE_MT_curveTypeHandles"
+        pie.operator("curve.cyclic_toggle")
+        pie.separator()
+        pie.operator("curve.switch_direction")
+        pie.separator()
+        pie.operator("wm.call_menu_pie", text='Select...').name = "SUBPIE_MT_curveSelect"
+
+    def draw_edit_gpencil_mode(self, pie, context):
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+    def draw_paint_gpencil_mode(self, pie, context):
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+    def draw_sculpt_gpencil_mode(self, pie, context):
+        pie.operator_enum("OBJECT_OT_mode_set", "mode")
+
+    def draw_paint_texture_mode(self, pie, context):
+        pie.operator("object.mode_set", text="object mode", icon="OBJECT_DATAMODE")
+        pie.separator()
+        box = pie.box()
+        brush = context.tool_settings.image_paint.brush
+        if brush.image_paint_capabilities.has_color:
+            UnifiedPaintPanel.prop_unified_color(box, context, brush, "color", text="")
+            UnifiedPaintPanel.prop_unified_color_picker(box, context, brush, "color", value_slider=True)
+        if brush.image_paint_capabilities.has_radius:
+            UnifiedPaintPanel.prop_unified(box, context, brush, "size", slider=True)
+            UnifiedPaintPanel.prop_unified(box, context, brush, "strength", slider=True)
+
+    def draw_paint_weight_mode(self, pie, context):
+        pie.operator("object.mode_set", text="object mode", icon="OBJECT_DATAMODE")
+        pie.separator()
+        box = pie.box()
+        brush = context.tool_settings.weight_paint.brush
+        UnifiedPaintPanel.prop_unified(box, context, brush, "weight", slider=True)
+        UnifiedPaintPanel.prop_unified(box, context, brush, "size", slider=True)
+        UnifiedPaintPanel.prop_unified(box, context, brush, "strength", slider=True)
+
+    def draw_sculpt_mode(self, pie, context):
+            # WEST
+            pie.operator("object.mode_set", text="object mode", icon="OBJECT_DATAMODE")
+            # EAST
+            #pie.prop(bpy.context.view_layer.objects.active, "use_dynamic_topology_sculpting", toggle=True)
+            pie.operator("sculpt.dynamic_topology_toggle", text="Dyntopo Toggle") #, icon="OBJECT_DATAMODE")
+            # SOUTH
+            box = pie.box()
+            #show the colour picker directly
+            brush = context.tool_settings.sculpt.brush
+            capabilities = brush.sculpt_capabilities
+
+            if capabilities.has_color:
+                split = box.split(factor=0.1)
+                UnifiedPaintPanel.prop_unified_color(split, context, brush, "color", text="")
+                UnifiedPaintPanel.prop_unified_color_picker(split, context, brush, "color", value_slider=True)
+                box.prop(brush, "blend", text="")
+
+            ups = context.tool_settings.unified_paint_settings
+            size = "size"
+            size_owner = ups if ups.use_unified_size else brush
+            if size_owner.use_locked_size == 'SCENE':
+                size = "unprojected_radius"
+
+            UnifiedPaintPanel.prop_unified(box, context, brush, size, 
+                                            unified_name="use_unified_size", pressure_name="use_pressure_size",
+                                            text="Radius",
+                                            slider=True,
+                                            )
+
+            UnifiedPaintPanel.prop_unified(box, context, brush,
+                                            "strength",
+                                            unified_name="use_unified_strength",
+                                            pressure_name="use_pressure_strength",
+                                            slider=True,
+                                            )
+
+            if capabilities.has_auto_smooth:
+                box.prop(brush, "auto_smooth_factor", slider=True)
+
+            if capabilities.has_normal_weight:
+                box.prop(brush, "normal_weight", slider=True)
+
+            if capabilities.has_pinch_factor:
+                text = "Pinch"
+                if brush.sculpt_tool in {'BLOB', 'SNAKE_HOOK'}:
+                    text = "Magnify"
+                box.prop(brush, "crease_pinch_factor", slider=True, text=text)
+
+            if capabilities.has_rake_factor:
+                box.prop(brush, "rake_factor", slider=True)
+
+            if capabilities.has_plane_offset:
+                box.prop(brush, "plane_offset", slider=True)
+                box.prop(brush, "plane_trim", slider=True, text="Distance")
+
+            if capabilities.has_height:
+                box.prop(brush, "height", slider=True, text="Height")
+
+    def draw_pose_mode(self, pie, context):
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+        pie.separator()
+        pie.menu("VIEW3D_MT_pose_context_menu", text="Pose Context Menu", icon="COLLAPSEMENU")
+        pie.separator()
+
+    def draw_edit_lattice_mode(self, pie, context):
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+        pie.separator()
+
+    def draw_edit_armature_mode(self, pie, context):
+        pie.operator("object.mode_set", icon="OBJECT_DATAMODE")
+        pie.separator()
+        pie.menu("VIEW3D_MT_edit_armature_names")
+        pie.separator()
+
+    def draw_paint_vertex_mode(self, pie, context):
+        pie.operator("object.mode_set", text="object mode", icon="OBJECT_DATAMODE")
+        pie.separator()
+        box = pie.box()
+        brush = context.tool_settings.vertex_paint.brush
+        UnifiedPaintPanel.prop_unified_color(box, context, brush, "color", text="")
+        UnifiedPaintPanel.prop_unified_color_picker(box, context, brush, "color", value_slider=True)
+        UnifiedPaintPanel.prop_unified(box, context, brush, "size", slider=True)
+        UnifiedPaintPanel.prop_unified(box, context, brush, "strength", slider=True)
+
+
+"""
+class VIEW3D_PIE_MT_mode(Menu):
     bl_label  = "Mode Selection"
 
     def draw(self, context):
@@ -542,7 +732,7 @@ class VIEW3D_PIE_MT_mode(Menu):
                 pressure_name="use_pressure_strength",
                 slider=True,
             )
-
+"""
 
 
 registry = [
