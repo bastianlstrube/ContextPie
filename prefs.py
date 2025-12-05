b3e7ac06a6209706cbee43c6fbc1e8edfa6a8e6d
Bastian Leonhardt Strube <bastian@Staalkasse.warbler-python.ts.net>
2025-12-05 22:45:01 +0100

    aligned to 3D Viewport Pies

--- prefs.py
+++ prefs.py
@@ -2,23 +2,26 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import io, platform, struct, urllib
-
 import bpy
 import addon_utils
-from bpy.types import AddonPreferences, Operator, KeyMap, KeyMapItem
+from bpy.types import AddonPreferences, KeyMap, KeyMapItem
 from bpy.props import BoolProperty
 from bl_ui.space_userpref import USERPREF_PT_interface_menus_pie
 
-from .blender_studio_utils.prefs import PrefsFileSaveLoadMixin, update_prefs_on_file, get_addon_prefs
+from .blender_studio_utils.prefs import get_addon_prefs
 from .blender_studio_utils.hotkeys import draw_hotkey_list
 
 class ContextPie_AddonPrefs(
-    PrefsFileSaveLoadMixin,
     AddonPreferences,
     USERPREF_PT_interface_menus_pie, # We use this class's `draw_centered` function to draw built-in pie settings.
 ):
     bl_idname = __package__
+
+    debug: BoolProperty(
+        name="Debug Mode",
+        default=False,
+        description="Enable some debugging UI",
+    )
 
     persistent_tools: BoolProperty(
         name="Persistent 'Knife' and 'Edge Loop' tools",
@@ -60,7 +63,7 @@ def button_draw_func(layout, km: KeyMap, kmi: KeyMapItem, compact=False):
     split = layout.split(factor=0.65, align=True)
     split.prop(kmi, "type", text="", full_event=True)
 
-    if kmi.idname != 'wm.call_menu_pie_drag_only':
+    if kmi.idname != 'wm.call_menu_pie_drag_only_cpie':
         return
     if not kmi.properties:
         sub.label(text="Missing properties. This should never happen!")
@@ -81,45 +84,4 @@ def button_draw_func(layout, km: KeyMap, kmi: KeyMapItem, compact=False):
     op.prop_name = 'on_drag'
 
 
-class WINDOW_OT_context_pie_prefs_save(Operator):
-    """Save Context Pie add-on preferences"""
-
-    bl_idname = "window.context_pie_prefs_save"
-    bl_label = "Save Pie Hotkeys"
-    bl_options = {'REGISTER'}
-
-    def execute(self, context):
-        filepath, data = update_prefs_on_file(context)
-        self.report({'INFO'}, f"Saved Pie Prefs to {filepath}.")
-        return {'FINISHED'}
-
-
-class WINDOW_OT_context_pie_prefs_load(Operator):
-    """Load Context Pie add-on preferences"""
-
-    bl_idname = "window.context_pie_prefs_load"
-    bl_label = "Load Pie Hotkeys"
-    bl_options = {'REGISTER'}
-
-    def execute(self, context):
-        prefs = get_addon_prefs(context)
-        filepath = prefs.get_prefs_filepath()
-        success = prefs.load_and_apply_prefs_from_file()
-
-        if success:
-            self.report({'INFO'}, f"Loaded pie preferences from {filepath}.")
-        else:
-            self.report({'ERROR'}, "Failed to load Pie preferences.")
-
-        return {'FINISHED'}
-
-
-registry = [
-    ContextPie_AddonPrefs,
-    WINDOW_OT_context_pie_prefs_save,
-    WINDOW_OT_context_pie_prefs_load,
-]
-
-
-def register():
-    ContextPie_AddonPrefs.register_autoload_from_file()
+registry = [ContextPie_AddonPrefs]
