dcbcc46f59cff4c6d10e7fcfee514c3ae46ac27e
Bastian Strube <bastian.strube@wombogames.com>
2026-05-27 15:21:41 +0200

    renamed op_node_utils

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ _delayed_loaded = False
 module_names = (
     "op_object_utils",
     "op_pie_wrappers",
-    "op_node_mode",
+    "op_node_utils",
     "hotkeys",
     "blender_studio_utils",
     "prefs",
